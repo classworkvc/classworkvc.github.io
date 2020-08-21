ada5b34ee996106e1f385b599aed4cc28251db00
iamtheretronerd <52290764+iamtheretronerd@users.noreply.github.com>
2020-08-21 23:02:11 +0530

    byebye

--- console/includes/footer.php
+++
@@ -1,7 +0,0 @@
- <footer class="foo">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">COPYRIGHT</i> 2020 <b >ClassWork</b></p>
-                </div>
-            </div>
-   </footer>
